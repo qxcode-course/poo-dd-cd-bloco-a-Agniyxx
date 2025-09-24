f4c7195ba77d9177ad89ddecd94633f91da34d46
Agne <agne.steph.moss@gmail.com>
2025-09-24 10:58:34 -0300

    iniciando  atividade - animal

---
+++ poo/database/animal/py/draft.py
@@ -0,0 +1,23 @@
+class Animal:
+    def __init__(self, species: str, sound: str):
+        self.species: str = species
+        self.sound: str = sound
+        self.age: int = 0
+    
+    def ageBy(self):
+
+    def makeSound(self):
+    
+    def warning()
+  
+
+    def __str__(self) -> str:
+        return f"Species:{self.species}, Age:{self.sound}, Sound:{self.age}"
+
+def main():
+    animal = Animal("", "")
+    while True:
+        line: str = input()
+        args: list[str] = line.split(" ")
+
+main()
