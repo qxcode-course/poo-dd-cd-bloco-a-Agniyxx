7ceb6c82f293bedde033248e9247a9a2418f48a4
Agne <agne.steph.moss@gmail.com>
2025-09-27 21:26:37 -0300

    resolução atividade animal - 4h

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -4,20 +4,39 @@ class Animal:
         self.sound: str = sound
         self.age: int = 0
     
-    def ageBy(self):
+    def ageBy(self, increment: int): #envelhecer
+        self.age += increment
+        if self.age >= 4:
+            print("warning: {species} morreu")
 
-    def makeSound(self):
-    
-    def warning()
-  
+    def makeSound(self) :
+        if self.sound == "0":
+            return ("")
+        if self.sound == "1":
+            return ("")
+        if self.sound == "2":
+            return ("")
+        if self.sound == "3":
+            return ("")
+        if self.sound == "4":
+            return "RIP"
+        return 0
 
     def __str__(self) -> str:
         return f"Species:{self.species}, Age:{self.sound}, Sound:{self.age}"
 
 def main():
-    animal = Animal("", "")
+    animal = Animal("","")
     while True:
         line: str = input()
-        args: list[str] = line.split(" ")
-
+        args: str = line.split(" ")
+        
+        if args[0] == "$end":
+            break
+        elif args[0] == "$new":
+            species = args[1]
+            sound = args[2]
+            animal = Animal(species, sound)
+        elif args[0] == "$show":
+            print(animal)
 main()
