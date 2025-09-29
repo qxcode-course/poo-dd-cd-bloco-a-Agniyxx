a93f2251bbdaa7bfc0189a8fb2e5ebfa93893fc2
Agne <agne.steph.moss@gmail.com>
2025-09-29 20:41:51 -0300

    continuando a atv carro

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,12 +1,30 @@
+gasMax: int = 100
+passMax: int = 2
+
 class Carro:
-    def __init__ (self, passageiros: int, km: int, gas: int):
-        self.passageiros: int = passageiros
-        self.km: int = km
-        self.gas: int = gas
-    
-
-
-
-
-    def __str__(self) -> str:
+    def __init__ (self, passageiros: int, km: int, gas: int, gasMax: int, passMax: int):
+        self.passageiros: int = 0
+        self.km: int = 0
+        self.gas: int = 0
+        self.gasMax: int = 100
+        self.passMax: int = 2
+ 
+     def __str__(self):
         return f"pass:{self.passageiros}, gas:{gas} km:{self.km}"
+    
+    def enter(self):
+        if self.passageiros >= self.passMax:
+            print("fail: limite de pessoas atingido")
+        else:
+             self.passageiros += 1
+    
+    def leave(self):
+        if self.passageiros <= 0:
+            print("fail: nao ha ninguem no carro")
+        else:
+            self.passageiros -= 1
+
+    def show(self):
+        print (self)
+
+    def main():
